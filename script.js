4ee80aee07ac99929dd3cf279ac3a2a6d5800752
ZenatyAh <zenatyah@gmail.com>
2025-08-27 11:48:59 +0300

    my solution , it's late but good

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function Student(name, age, grade) {
-  this.name = name;
-  this.age = age;
-  let gra = grade;
-  if (gra !== "A" && gra !== "F") {
-    gra = "F";
-  }
-  this.getGrade = function () {
-    return gra;
-  };
-  this.setGrade = function (newGrade) {
-    if (newGrade == "A" || newGrade == "F") {
-      gra = newGrade;
-    } else {
-      console.log("the grade should be only A or F");
+// Task Two
+function Student(name, age){
+    this.name = name;
+    this.age = age;
+    let grade;
+    // Methods
+    this.setGrade =  (grade) => {
+        if(grade === 'A' || grade === 'F'){
+            this.grade = grade;
+        }
+        else{
+            return 'value not accepted'
+        }
     }
-  };
+    this.getGrade =  (grade) => {
+        return this.grade;
+    }
+} 
+Student.prototype.introduce = function() {
+    console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${this.getGrade()}.`);
 }
-Student.prototype.introduce = function () {
-  console.log(
-    `Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${this.getGrade()}.`
-  );
-};
-const student = new Student("Mousa", 20, "A");
-student.introduce();
-console.log(student.getGrade());
-student.setGrade("F");
-student.introduce();
-student.setGrade("B");
+
+const StudentInstance = new Student('ahmed',21);
+console.log(StudentInstance);
+console.log(StudentInstance.setGrade(52));
+console.log(StudentInstance.getGrade());
+StudentInstance.introduce();
+
+
+
